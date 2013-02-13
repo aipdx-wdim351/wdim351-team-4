f9a92daa21ac4910986783dbb67491190b5a0d5f
Jace <jacepp@hotmail.com>
2013-02-12 20:00:08 -0800

    it works

--- docs/assets/js/mongolab.js
+++ docs/assets/js/mongolab.js
@@ -1,16 +1,37 @@
-angular.module('mongolab', ['ngResource']).
-  factory('Project', function($resource) {
-    var Project = $resource('https://api.mongolab.com/api/1/databases' +
-        '/klutzy-kalyn/collections/Comments/:id',
-        { apiKey: 'rFPd1l3nT5_5AhS9IplSXgdECvyBUcCn' }, {
-          update: { method: 'PUT' }
-        }
+angular.module('mongolabResource', ['ngResource']).factory('$mongolabResource', ['$resource', 'API_KEY', 'DB_NAME', function ($resource, API_KEY, DB_NAME) {
+
+  function MmongolabResourceFactory(collectionName) {
+
+    var resource = $resource('https://api.mongolab.com/api/1/databases/' + DB_NAME + '/collections/Comments/:id',
+      { apiKey:API_KEY, id:'@_id.$oid'}, { update:{ method:'PUT' } }
     );
 
-    Project.prototype.update = function(cb) {
-      return Project.update({id: this._id.$oid},
-          angular.extend({}, this, {_id:undefined}), cb);
+    resource.getById = function (id, cb, errorcb) {
+      return resource.get({id:id}, cb, errorcb);
     };
 
-    return Project;
-  });
+    resource.prototype.update = function (cb, errorcb) {
+      return resource.update({id:this._id.$oid}, angular.extend({}, this, {_id:undefined}), cb, errorcb);
+    };
+
+    resource.prototype.save = function (savecb, updatecb, errorSavecb, errorUpdatecb) {
+      if (this._id && this._id.$oid) {
+        return this.update(updatecb, errorUpdatecb);
+      } else {
+        return this.$save(savecb, errorSavecb);
+      }
+    };
+
+    resource.prototype.remove = function (cb, errorcb) {
+      return resource.remove({id:this._id.$oid}, cb, errorcb);
+    };
+
+    resource.prototype['delete'] = function (cb, errorcb) {
+      return this.remove(cb, errorcb);
+    };
+
+    return resource;
+  }
+
+  return MmongolabResourceFactory;
+}]);
