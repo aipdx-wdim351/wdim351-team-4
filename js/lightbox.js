5ad6bb92a85436c8000d2c1924cf5ec1fd25cbb0
Jace <jacepp@hotmail.com>
2013-02-20 19:56:38 -0800

    lightbox + wireframes

--- js/lightbox.js
+++ js/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'docs/assets/img/loading.gif';
-      this.fileCloseImage = 'docs/assets/img/close.png';
+      this.fileLoadingImage = 'images/loading.gif';
+      this.fileCloseImage = 'images/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
